e08b639e30a66096aa0ae7a40390a4130713e483
deboragois <debora@torresetorres.com>
2024-09-13 12:22:45 +0200

    Add migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
-  validates :name, presence: true
+  validates :title, presence: true
   validates :author, presence: true
+  validates :isbn, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_13_090559) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_13_101835) do
   create_table "books", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.string "author"
     t.text "description"
     t.decimal "price"
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "isbn"
   end
 
 end
